e399ece09317771d091d959139b7d0dea519f420
Artem Matveev <matveev@rutoken.ru>
2026-03-27 17:02:36 +0300

    Move gost_crypt functions impl from gost-core to engine-related translation unit

---
+++ gost_eng_crypt.h
@@ -0,0 +1,12 @@
+#ifndef GOST_ENG_CRYPT_H
+#define GOST_ENG_CRYPT_H
+
+#include <openssl/evp.h>
+#include <openssl/types.h>
+
+#include "gost_lcl.h"
+
+EVP_CIPHER *GOST_init_cipher(GOST_cipher *c);
+void GOST_deinit_cipher(GOST_cipher *c);
+
+#endif
