845a2e73e1e15b5f7214b571af02969844223861
Artem Matveev <matveev@rutoken.ru>
2026-04-07 14:13:42 +0300

    Add compile flag for engine legacy code

--- .github/build-engine.sh
+++ .github/build-engine.sh
@@ -3,14 +3,18 @@
 source $(dirname "${BASH_SOURCE[0]}")/config.sh
 
 PATH=$OPENSSL_INSTALL_PREFIX/bin:$PATH
+GOST_ENABLE_LEGACY=${GOST_ENABLE_LEGACY:-OFF}
 
 mkdir -p build
 cd build
 cmake -DTLS13_PATCHED_OPENSSL=$PATCH_OPENSSL -DOPENSSL_ROOT_DIR=$OPENSSL_INSTALL_PREFIX \
-    -DOPENSSL_ENGINES_DIR=$OPENSSL_INSTALL_PREFIX/engines ${ASAN-} ..
+    -DOPENSSL_ENGINES_DIR=$OPENSSL_INSTALL_PREFIX/engines ${ASAN-} \
+    -DGOST_ENABLE_LEGACY=$GOST_ENABLE_LEGACY  ..
 make
 make test CTEST_OUTPUT_ON_FAILURE=1
 if [ -z "${ASAN-}" ]; then
-    make tcl_tests_engine
+    if [ "$GOST_ENABLE_LEGACY" = "ON" ]; then
+        make tcl_tests_engine
+    fi
     make tcl_tests_provider
 fi
